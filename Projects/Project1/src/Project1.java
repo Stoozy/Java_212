cb425a34abd92cb8f44fe9eae9576c6123e4e08b
Stoozy <mahinsemail@gmail.com>
2020-03-04 19:59:27 -0500

    Project1 updated

--- Projects/Project1/src/Project1.java
+++ Projects/Project1/src/Project1.java
@@ -1,16 +1,65 @@
+import java.util.StringTokenizer;
+
 public class Project1 {
 
     public static void main(String[] args){
-        Clock c = new Clock(10, 23, 50);
+//        Clock c = new Clock(10, 23, 50);
 //        System.out.print(c.toString());
+        Clock[] c = new Clock[50];
+        Clock[] d = new Clock[50];
 
         String[] lines = new String[50];
 
-        inputFromFile(args[0], lines);
+        int size = inputFromFile(args[0], lines);
 
-        for(int i=0; i < inputFromFile(args[0], lines); i++ ){
-            System.out.println(lines[i]);
+        for(int i=0; i < size; i++ ){
+            StringTokenizer tokens = new StringTokenizer(lines[i], ":");
+            int[] time = new int[3];
+
+
+            int k=0;
+            while(tokens.hasMoreTokens()){
+                time[k] = Integer.parseInt(tokens.nextToken());
+                k++;
+            }
+
+            c[i] = new Clock(time[0], time[1], time[2]);
+
         }
+
+
+
+
+        for(int j = 0; j< size; j++){
+            d[j] = c[j];
+            System.out.println(c[j].toString());
+        }
+
+    }
+
+    public void selectionSort(int[] arr, int size){
+        int min_idx=0;
+
+        for(int i = 0; i< size ; i++){
+            if(arr[min_idx] > arr[i]  ){
+                min_idx = i;
+            }
+
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+            
+        }
+
+
+
+    }
+
+    public void swap(int x, int y){
+        int temp = y;
+        y = x;
+        x = temp;
+
     }
 
     private static int inputFromFile(String filename, String[] words){
